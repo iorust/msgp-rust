69e2c86c9a2aa28e1af1c41e03d7e5dec7a0548d
zensh <admin@zensh.com>
2016-07-12 20:19:51 +0800

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,139 @@ pub fn encode(val: &mut Vec<u8>) -> Vec<u8> {
     }
 }
 
+pub fn encode_slice(val: &[u8]) -> Vec<u8> {
+    let len: usize = val.len();
+    assert!(len < 2097152);
+
+    if len < 128 {
+        let mut res: Vec<u8> = Vec::with_capacity(len + 1);
+        res.push(len as u8);
+        res.extend_from_slice(val);
+        return res;
+    } else if len < 16384 {
+        let mut res: Vec<u8> = Vec::with_capacity(len + 2);
+        let len = len as u16;
+        res.push(((len >> 7) | 0x80u16) as u8);
+        res.push((len & 0x7Fu16) as u8);
+        res.extend_from_slice(val);
+        return res;
+    } else {
+        let mut res: Vec<u8> = Vec::with_capacity(len + 3);
+        let len = len as u32;
+        res.push((((len >> 14) & 0x7Fu32) as u8) | 0x80u8);
+        res.push((((len >> 7) & 0x7Fu32) as u8) | 0x80u8);
+        res.push((len & 0x7Fu32) as u8);
+        res.extend_from_slice(val);
+        return res;
+    }
+}
+
+pub fn decode(val: &Vec<u8>) -> Option<Vec<u8>> {
+    if val.len() == 0 { return None; }
+    if let Some(res) = parse_buffer(val, 0) {
+        let mut buf: Vec<u8> = Vec::with_capacity(res.1 - res.0);
+        buf.extend_from_slice(&val[res.0..res.1]);
+        return Some(buf);
+    }
+    return None;
+}
+
+/// A streaming Msgp decoder.
+#[derive(Debug)]
+pub struct Decoder {
+    pos: usize,
+    msg_start: usize,
+    msg_end:usize,
+    buf: Vec<u8>,
+    res: Vec<Vec<u8>>,
+}
+
+impl Decoder {
+    pub fn new() -> Self {
+        Decoder {
+            pos: 0,
+            msg_start: 0,
+            msg_end: 0,
+            buf: Vec::new(),
+            res: Vec::with_capacity(8),
+        }
+    }
+
+    pub fn feed(&mut self, buf: &Vec<u8>) {
+        self.buf.extend(buf);
+        self.parse();
+    }
+
+    /// Reads a decoded massage buffer, will return `None` if no buffer decoded.
+    pub fn read(&mut self) -> Option<Vec<u8>> {
+        if self.res.len() == 0 {
+            return None;
+        }
+        Some(self.res.remove(0))
+    }
+
+    /// Returns the buffer's length that wait for decoding. It usually is `0`. Non-zero means that
+    /// decoder need more buffer.
+    pub fn buffer_len(&self) -> usize {
+        self.buf.len()
+    }
+
+    /// Returns decoded massages count. The decoded messages will be hold by decoder,
+    /// until you read them.
+    pub fn result_len(&self) -> usize {
+        self.res.len()
+    }
+
+    fn prune_buf(&mut self) {
+        if self.pos == self.buf.len() {
+            self.pos = 0;
+            self.buf.clear();
+        }
+    }
+
+    fn parse(&mut self) {
+        if self.msg_end > self.buf.len() { return; }
+        if self.msg_start > self.pos {
+            let mut buf: Vec<u8> = Vec::with_capacity(self.msg_end - self.msg_start);
+            buf.extend_from_slice(&self.buf[self.msg_start..self.msg_end]);
+            self.res.push(buf);
+            self.pos = self.msg_end;
+            self.msg_start = self.msg_end;
+            return self.parse();
+        }
+        if self.buf.len() <= self.pos {
+            return self.prune_buf();
+        }
+
+        match parse_buffer(&self.buf, self.pos) {
+            Some((start, end)) => {
+                self.msg_start = start;
+                self.msg_end = end;
+                if start == end {
+                    self.pos = start;
+                }
+                self.parse();
+            }
+            None => {}
+        }
+    }
+}
+
+fn parse_buffer(buffer: &Vec<u8>, offset: usize) -> Option<(usize, usize)> {
+    let mut byte: u8 = 0;
+    let mut length: usize = 0;
+    let mut offset: usize = offset;
+    loop {
+        let len = *buffer.get(offset).unwrap() as usize;
+        offset += 1;
+        if len < 128 { return Some((offset, offset + length + len)); }
+        byte += 1;
+        if byte >= 4 { panic!("Max buffer length must be small than 268435456"); }
+        if offset >= buffer.len() { return None; }
+        length = ((len & 0x7f) + length) * 128
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -113,6 +246,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn encode_len_lt_268435456() {
         let mut res = Vec::with_capacity(2097153 + 4);
         res.extend_from_slice(&[0x81u8, 0x80u8, 0x80u8, 0x01u8]);
@@ -123,5 +257,18 @@ mod tests {
         res.extend_from_slice(&[0xffu8, 0xffu8, 0xffu8, 0x7fu8]);
         res.append(&mut vec![0xffu8; 268435455]);
         assert_eq!(encode(&mut vec![0xffu8; 268435455]), res);
+    }
+
+    #[test]
+    fn decode_buf() {
+        let res = decode(&encode(&mut vec![0xffu8; 16384]));
+        assert_eq!(res.unwrap(), vec![0xffu8; 16384]);
+    }
+
+    #[test]
+    fn decoder_sim() {
+        let mut decoder = Decoder::new();
+        decoder.feed(&encode(&mut vec![0xffu8; 16384]));
+        assert_eq!(decoder.read().unwrap(), vec![0xffu8; 16384]);
     }
 }
